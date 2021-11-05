5031fca2cdc5eaa1d58022706a6f640b9ed64d66
tanyo777 <87861727+tanyo777@users.noreply.github.com>
2021-11-05 08:38:31 +0200

    Update app.js
    Bus scheduler

--- 02.Bus-Schedule/app.js
+++ 02.Bus-Schedule/app.js
@@ -1,17 +1,40 @@
 function solve() {
+  let info = document.querySelector(".info");
+  let departInfo = document.getElementById("depart");
+  let arriveInfo = document.getElementById("arrive");
 
-    function depart() {
-        console.log('Depart TODO...');
+  let next = "";
+  let stopName = "";
+  async function depart() {
+    try {
+      let url = `http://localhost:3030/jsonstore/bus/schedule/${next}`;
+      let response = await fetch(url);
+      if (response.status !== 200 || next.length === 0) {
+        throw new Error("Error");
+      }
+      let reponseJson = await response.json();
+      stopName = reponseJson.name;
+      info.textContent = `Next stop ${stopName}`;
+      next = reponseJson.next;
+      departInfo.disabled = true;
+      arriveInfo.disabled = false;
+    } catch (err) {
+      info.textContent = err.message;
+      departInfo.disabled = true;
+      arriveInfo.disabled = true;
     }
+  }
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+  function arrive() {
+    info.textContent = `Arriving at ${stopName}`;
+    departInfo.disabled = false;
+    arriveInfo.disabled = true;
+  }
 
-    return {
-        depart,
-        arrive
-    };
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
