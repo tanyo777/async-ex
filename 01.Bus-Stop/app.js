69a7dce6e685dab0a061260ed611a0288b232bda
tanyo777 <87861727+tanyo777@users.noreply.github.com>
2021-11-05 08:37:16 +0200

    Update app.js

--- 01.Bus-Stop/app.js
+++ 01.Bus-Stop/app.js
@@ -1,3 +1,37 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+  const busId = document.getElementById("stopId");
+  const stopName = document.getElementById("stopName");
+  const ul = document.getElementById("buses");
+  try {
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${busId.value}`;
+    let response = await fetch(url);
+    if (response.status !== 200 || busId.value.length === 0) {
+      throw new Error("Error");
+    } else {
+      let json = await response.json();
+      onSuccess(json);
+    }
+  } catch (err) {
+    onError(err);
+  }
+
+  function onError(err) {
+    stopName.textContent = err.message;
+    ul.innerHTML = "";
+    busId.value = "";
+  }
+
+  function onSuccess(json) {
+    stopName.textContent = json.name;
+    let buses = json.buses;
+    let keys = Object.keys(buses);
+    for (let key of keys) {
+      let li = document.createElement("li");
+      let context = `Bus ${key} arrives in ${buses[key]} minutes`;
+      li.textContent = context;
+      ul.appendChild(li);
+    }
+    busId.value = "";
+  }
 }
+
